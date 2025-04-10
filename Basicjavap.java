c0e8db73c1e644b1a535332bbaf59c292b33e878
daminigupta123 <damini.cs231079@global.org.in>
2025-04-10 13:59:46 +0530

    day2

---
+++ Basicjavap.java
@@ -0,0 +1,90 @@
+public class Basicjavap{
+    public  static void main(String[]args) 
+    {
+        
+          byte a=20;
+        byte b=30;
+        int c=a+b;  
+    System.out.println(c);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
