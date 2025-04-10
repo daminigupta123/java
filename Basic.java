b0d9f3eecc2eac93f09d41986f0922a54cd76eb5
daminigupta123 <damini.cs231079@global.org.in>
2025-04-10 13:40:25 +0530

    msg

---
+++ Basic.java
@@ -0,0 +1,100 @@
+public class Basic{
+    public  static void main(String[]args) 
+    {
+        
+         /*  byte a=20;
+        byte b=30;
+        int c=a+b;  */
+        boolean a=true;
+        if(a==true){
+            System.out.println("damini");
+        }
+            else
+            System.out.println("harshita");
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+
+}
