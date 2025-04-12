4a81dc5196ee5bb81ef7716c10e428eecdb6fd66
damini <damini.cs231079.global.org.in>
2025-04-12 11:52:41 +0530

    b

---
+++ greater.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class greater {
+   public static void main(String[] args) { 
+        System.out.println("enter first number");   
+        Scanner inp=new Scanner(System.in);
+                        int  a=inp.nextInt();
+                        System.out.println("enter second number");
+                Scanner in =new Scanner(System.in);
+                int b=in.nextInt();
+                if(a>b) 
+                System.out.println("a is greater");
+                else 
+                System.out.println("b is greater");
+
+    
+}}
