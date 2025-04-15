d7c04c872c504053ec13f70cffb7e6e5c2891b2b
damini <damini.cs231079.global.org.in>
2025-04-15 13:40:06 +0530

    pattern

--- pattern.java
+++ pattern.java
@@ -4,11 +4,20 @@ public class pattern {
         for(int i=1;i<=5;i++)
         {
         
-            for(int j=1;j<=i;j++)
+            for(int j=5;j>=i;j--)
             {
-                System.out.print("# ");
+                System.out.print(" ");
             }
-            System.out.println("");
+            for(int k=1;k<=i;k++)
+            {
+            System.out.print("#");
+        } 
+        for(int p=1;p<i;p++)
+        {
+            System.out.print("#");
         }
+        System.out.println();
+    }
+
     }
 }
