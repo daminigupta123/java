44627a92953e55214ac0c41d8958c7b7a619b942
damini <damini.cs231079.global.org.in>
2025-04-15 13:19:56 +0530

    pattern

---
+++ buton.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class buton {
+    public static void main(String[] args)
+    {
+Scanner sc=new Scanner(System.in);
+System.out.println("enter buton number");
+int a=sc.nextInt();
+switch (a) {
+    case 1:
+    System.out.println("hello");
+        break;
+     case 2:   System.out.println("namste");break;
+    default:
+    System.out.println("kanjoor");
+        break;
+}
+    
+
+   
+
+   
+
+    }
+}
