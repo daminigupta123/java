9ecefd666f360e89a360f03c91a61978a0530e07
damini <damini.cs231079.global.org.in>
2025-04-16 13:33:43 +0530

    array

--- array.java
+++ array.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 public class array {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size of array");
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        System.out.println("enter valuse of array");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
         
+        }
+       /* int a=sc.nextInt();
         int[] arr={1,2,3,4,5};
         int a=50;
         System.out.println(a);
-        for(int i=0;i<arr.length;i++)
-        System.out.println(arr[i]);
+         */
+        for(int k=0;k<arr.length;k++)
+        System.out.println(arr[k]);
     }
 }
+
