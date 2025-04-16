579045f44059cfda1cb1cd5a1e3441fcbf5a4568
damini <damini.cs231079.global.org.in>
2025-04-16 13:13:28 +0530

    array

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args) {
+        
+        int[] arr={1,2,3,4,5};
+        int a=50;
+        System.out.println(a);
+        for(int i=0;i<arr.length;i++)
+        System.out.println(arr[i]);
+    }
+}
