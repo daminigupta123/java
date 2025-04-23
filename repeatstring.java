3b1e79fb233e6e2c29615bffbea3f8624a490739
damini <damini.cs231079.global.org.in>
2025-04-23 13:19:02 +0530

    trem

--- repeatstring.java
+++ repeatstring.java
@@ -1,13 +1,17 @@
 public class repeatstring {
   
     public static void main(String[] args) {
-        String str="damini";
+        String str2="damini";
+        String  str =new String("hello");
+        String str1=new String("hello");
+        System.out.println(str==str1);
         String newstr="";
         for(int i=0;i<str.length();i++){
         char c=str.charAt(i);
         if(newstr.indexOf(c)==-1){
             newstr=newstr+c;
         }
+    
 
     }
     System.out.println(newstr+"  ");
