e5a318fef698c0b667121d40880556579e48fe63
damini <damini.cs231079.global.org.in>
2025-04-29 15:17:05 +0530

    a

---
+++ ractan.java
@@ -0,0 +1,18 @@
+class area{
+    int l=20,w=10;
+    int are=l*w;
+    void cal(){
+       System.out.println(are);
+    }
+}
+
+
+public class ractan {
+    public static void main(String[] args) {
+        
+        area ar=new area();
+        ar.cal();
+    }
+
+    
+}
