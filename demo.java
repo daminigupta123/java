9f5555db6ace9c3a8fde391ad8da40de14bc0ccf
damini <damini.cs231079.global.org.in>
2025-05-21 14:01:01 +0530

    demo class

---
+++ demo.java
@@ -0,0 +1,22 @@
+ interface A {
+void display();  
+}
+ interface b {
+    void display();
+    
+}
+class demoA implements  A, b{
+
+public void display()
+{
+    System.out.println("it is demo class");
+}
+
+}
+public class demo {
+    public static void main(String[] args) {
+        demoA obj=new demoA();
+        obj.display();
+    }
+    
+}
