e3d495a8835c62d2c0be0b8296a30494ac9fc4fc
damini <damini.cs231079.global.org.in>
2025-05-28 15:39:15 +0530

    count fq of the string

--- viewport.java
+++ viewport.java
@@ -3,8 +3,17 @@ import java.util.HashMap;
 public class viewport {
 
     public static void main(String[] args) {
-        String str ="programing";
+        String str ="programing is fun and fun is programing ";
         char[] ch=str.toCharArray();
-        HashMap<Character,Integer> map=new HashMap<>();
+        HashMap<String ,Integer> map=new HashMap<>();
+        String[] newstr=str.split(" ");
+       
+       for(String i:newstr){
+        map.put(i,  map.getOrDefault(i,0)+1);
+       }
+       System.out.println(map);
+   
+      
+             
     }
 }
