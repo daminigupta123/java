0f38f7f61b482cbffcf7633af126d07c4ca69d2e
damini <damini.cs231079.global.org.in>
2025-06-06 09:06:59 +0530

    leaders in array

---
+++ leaders.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class leaders {
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        printLeaders(arr);
+    }
+
+    public static void printLeaders(int[] arr) {
+        int n = arr.length;
+        int maxFromRight = arr[n - 1];
+        System.out.print("Leaders: " + maxFromRight + " ");
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > maxFromRight) {
+                maxFromRight = arr[i];
+                System.out.print(maxFromRight + " ");
+            }
+        }
+        System.out.println();
+    }
+}
