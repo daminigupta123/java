a0efabc8b2c708b5c0a281506c9b30b13c0ccdf3
damini <damini.cs231079.global.org.in>
2025-06-09 10:57:50 +0530

    string start with a or A

---
+++ startwitha.java
@@ -0,0 +1,30 @@
+
+ import java.util.*;
+public class startwitha {
+    public static List<Integer> getWordLengths(List<String> words) {
+        List<Integer> lengths = new ArrayList<>();
+        for (String word : words) {
+            lengths.add(word.length());
+        }
+        return lengths;
+    }
+    public static int countStartsWithA(List<String> words) {
+        int count = 0;
+        for (String word : words) {
+            if (!word.isEmpty() && (word.charAt(0) == 'A' || word.charAt(0) == 'a')) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("damini", "arshita", "vanshika", "astha", "Anu", "Anurag");
+        List<Integer> lengths = getWordLengths(words);
+        System.out.println("Word lengths: " + lengths);
+
+        int countA = countStartsWithA(words);
+        System.out.println("Number of strings starting with 'A' or 'a': " + countA);
+    }
+}
+    
+
