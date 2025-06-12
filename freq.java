d743bc76da6d54418a9e4e23beaf801d8e1b7114
damini <damini.cs231079.global.org.in>
2025-06-12 10:44:31 +0530

    print spiral way

--- freq.java
+++ freq.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class freq{
     public static void main(String[] args) {
-        String str = "damini";
+        String str = "harshitadamini";
         Map<Character, Integer> freq = new HashMap<>();
         for (char c : str.toCharArray()) {
             freq.put(c, freq.getOrDefault(c, 0) + 1);
