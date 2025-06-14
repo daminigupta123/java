6ed7e758417212c57e4951f3bfcbad77e4b8bc81
damini <damini.cs231079.global.org.in>
2025-06-14 21:44:39 +0530

    reverse a string

---
+++ .vscode/.vscode/reverse.java
@@ -0,0 +1,8 @@
+    public class reverse {
+    public static void main(String[] args) {
+        String str = "hello";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("Reversed string: " + reversed);
+    }
+}
+
