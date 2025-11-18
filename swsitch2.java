01b5b16721ac145893a590319c5cba2c9a627bc2
Damini Gupta <damini.cs231079@global.org.in>
2025-11-18 10:38:17 +0530

    switch2

---
+++ swsitch2.java
@@ -0,0 +1,27 @@
+public class swsitch2 {
+    
+    // Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+
+   swsitch2() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 5;
+      switch (var1) {
+         case 1:
+         case 2:
+         case 3:
+         case 4:
+            System.out.println("hello");
+            break;
+         case 5:
+            System.out.println("hii");
+            break;
+         default:
+            System.out.println("byye");
+      }
+
+   }
+}
+
+
